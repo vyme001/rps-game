8058b55950d63be132640f33e712e0613e134ae5
vyme001 <250090050+vyme001@users.noreply.github.com>
2026-02-18 17:42:21 -0500

    Define the possible positions with their paired logic position for win/lose. use the keys to determine the pc position using a random num val based on the length of the array

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log('test connection');
+
+
+/*
+OBJECT
+- Where the name/positons live within a defined rules set
+*/
+const posRules = {
+    "rock":"scissors",
+    "scissors":"paper",
+    "paper":"rock",
+};
+
+/*
+FUNCTION:
+- Get value for pc position on start up
+*/
+
+function pcPosition(){
+    const arr = Object.keys(posRules);
+    return arr[Math.floor(Math.random() * arr.length)];    
+};
+console.log(pcPosition());
